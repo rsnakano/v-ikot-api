ac57be21b9a4389a0d547f07243593d2793e2b5f
Ryosuke Nakano <rsnakano@up.edu.ph>
2023-03-21 15:00:43 +0800

    Adds poi migration table

---
+++ database/migrations/2023_03_20_142258_create_pois_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pois', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('history');
+            $table->string('courses');
+            $table->string('offices');
+            $table->float('top', 8, 5);
+            $table->float('bottom', 8, 5);
+            $table->float('left', 8, 5);
+            $table->float('right', 8, 5);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pois');
+    }
+};
